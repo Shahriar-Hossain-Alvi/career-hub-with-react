1dfeb0f135f64d78b7444e7dfcbaafb9272ff1c0
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-24 15:44:00 +0600

    added assets

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>   
-            <h2>Oops!</h2> 
+        <div className="flex flex-col absolute top-1/2 left-1/2 text-center gap-10">   
+            <h2 className="text-7xl">Oops!</h2> 
+            <Link to='/'><button className="btn btn-lg btn-error text-white">Back To Home</button></Link>
         </div>
     );
 };
